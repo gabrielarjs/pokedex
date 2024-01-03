5b5c6d1143d8a2968f8161511c2911a728084ea9
araujoifsp <araujo.sousa@aluno.ifsp.edu.br>
2024-01-02 22:44:20 -0300

    request

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10";
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))    
+    .catch((error) => console.log(error));
+
+
+ 
