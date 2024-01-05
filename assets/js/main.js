a296a3dbcd448fbbb99c8fbdc3e0dd6d6b1b1d4a
gabrielarjs <gabriiel.sousa161@gmail.com>
2024-01-05 14:28:10 -0300

    inserindo tipo nome e imagem

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,27 @@
-const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10";
+function convertPokemonTypesToLi(pokemonTypes){
+    return pokemonTypes.map((type) => `<li class="type">${type.type.name}</li>`);
+}
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))    
-    .catch((error) => console.log(error));
+function pokemonToHtml(pokemon){
+    return `
+    <li class="pokemon">
+        <span class="number">${pokemon.order}</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                ${convertPokemonTypesToLi(pokemon.types).join("")}
+            </ol>
+            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+        </div> 
+    </li>`;
+}
+
+const lista = document.querySelector(".pokemons");
+
+pokeApi.getPokemons(0, 10).then((pokemons = []) => {
+    lista.innerHTML += pokemons.map(pokemonToHtml).join("");
+})
 
 
  
